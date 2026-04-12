991e14a8bbc7f639559637a83f59051d7c6197e1
Prashanth Kamath <prashanth_kamath_2001@hotmail.com>
2026-04-12 16:22:15 +0530

    Set allowed hosts in Vite config
    Update Vite configuration to specify allowed hosts.Set allowed hosts in Vite config......

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,12 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: port,
-        host: '0.0.0.0',
+        allowedHosts: ['mywebapp04-gudebcc4bafdg8bn.southindia-01.azurewebsites.net', 'localhost'],
+        // host: '0.0.0.0',
       },
       preview: {
         port: port,
-        host: '0.0.0.0',
+        // host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
